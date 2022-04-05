e066513bbdc0857db639b964d3e9faa84695f923
Dusan Djordjevic <dussann1997@live.com>
2022-04-05 20:45:31 +0200

    Finished movie showcase widget

--- lkc-elementor-widgets.php
+++ lkc-elementor-widgets.php
@@ -4,257 +4,246 @@ Plugin Name: Lkc Elementor Widgets
 Version: 1.0.0
 Plugin URI: https://resize.rs/
 Description: Custom Lkc elementor widgets.
-Author: LKC TEAM
+Author: Resize Team
 Author URI: https://resize.rs/
 */
 
+use Elementor\Cinema_Movies_Showcase_Widget;
 use Elementor\Widgets_Manager;
 
-if (!defined('ABSPATH')) {
-    exit; // Exit if accessed directly.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
 }
 
-if (!defined('LKC_PLUGIN_FILE')) {
-    define('LKC_PLUGIN_FILE', __FILE__);
+if ( ! defined( 'LKC_PLUGIN_FILE' ) ) {
+	define( 'LKC_PLUGIN_FILE', __FILE__ );
 }
 
-class Lkc_Elementor_Widgets
-{
+class Lkc_Elementor_Widgets {
 
-    /**
-     * Plugin Version
-     *
-     * @since 1.0.0
-     *
-     * @var string The plugin version.
-     */
-    const VERSION = '1.0.0';
+	/**
+	 * Plugin Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string The plugin version.
+	 */
+	const VERSION = '1.0.0';
 
-    /**
-     * Minimum Elementor Version
-     *
-     * @since 1.0.0
-     *
-     * @var string Minimum Elementor version required to run the plugin.
-     */
-    const MINIMUM_ELEMENTOR_VERSION = '2.5.11';
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.5.11';
 
-    /**
-     * Minimum PHP Version
-     *
-     * @since 1.0.0
-     *
-     * @var string Minimum PHP version required to run the plugin.
-     */
-    const MINIMUM_PHP_VERSION = '6.0';
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '6.0';
 
-    /**
-     * Instance
-     *
-     * @since 1.0.0
-     *
-     * @access private
-     * @static
-     *
-     * The single instance of the class.
-     */
-    protected static $instance = null;
+	/**
+	 * Instance
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access private
+	 * @static
+	 *
+	 * The single instance of the class.
+	 */
+	protected static $instance = null;
 
-    public static function get_instance()
-    {
-        if (!isset(static::$instance)) {
-            static::$instance = new static;
-        }
+	public static function get_instance() {
+		if ( ! isset( static::$instance ) ) {
+			static::$instance = new static;
+		}
 
-        return static::$instance;
-    }
+		return static::$instance;
+	}
 
-    /**
-     * Constructor
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
+	/**
+	 * Constructor
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
 
-    protected function __construct()
-    {
-        // Check if Elementor installed and activated
-        if (!did_action('elementor/loaded')) {
-            add_action('admin_notices', [$this, 'admin_notice_missing_main_plugin']);
+	protected function __construct() {
+		// Check if Elementor installed and activated
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_missing_main_plugin' ] );
 
-            return;
-        }
+			return;
+		}
 
-        // Check for required PHP version
-        if (version_compare(PHP_VERSION, self::MINIMUM_PHP_VERSION, '<')) {
-            add_action('admin_notices', [$this, 'admin_notice_minimum_php_version']);
+		// Check for required PHP version
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			add_action( 'admin_notices', [ $this, 'admin_notice_minimum_php_version' ] );
 
-            return;
-        }
+			return;
+		}
 
-        $this->enqueue_widgets();
+		$this->enqueue_widgets();
 
-        // Register Widget Categories
-        add_action('elementor/elements/categories_registered', [$this, 'register_widget_categories']);
-    }
+		// Register Widget Categories
+		add_action( 'elementor/elements/categories_registered', [ $this, 'register_widget_categories' ] );
+	}
 
-    /**
-     * Admin notice
-     *
-     * Warning when the site doesn't have Elementor installed or activated.
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
-    public function admin_notice_missing_main_plugin()
-    {
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have Elementor installed or activated.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_missing_main_plugin() {
 
-        if (isset($_GET['activate'])) {
-            unset($_GET['activate']);
-        }
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
 
-        $message = sprintf(
-        /* 1: Plugin name 2: Elementor */
-            esc_html__('"%1$s" requires "%2$s" to be installed and activated.', 'lkc-elementor-widgets'),
-            '<strong>' . esc_html__('Lkc Elementor Widgets', 'lkc-elementor-widgets') . '</strong>',
-            '<strong>' . esc_html__('Elementor', 'lkc-elementor-widgets') . '</strong>'
-        );
+		$message = sprintf(
+		/* 1: Plugin name 2: Elementor */
+			esc_html__( '"%1$s" requires "%2$s" to be installed and activated.', 'lkc-elementor-widgets' ),
+			'<strong>' . esc_html__( 'Lkc Elementor Widgets', 'lkc-elementor-widgets' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'lkc-elementor-widgets' ) . '</strong>'
+		);
 
-        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 
-    }
+	}
 
-    /**
-     * Admin notice
-     *
-     * Warning when the site doesn't have a minimum required Elementor version.
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
-    public function admin_notice_minimum_elementor_version()
-    {
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required Elementor version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_elementor_version() {
 
-        if (isset($_GET['activate'])) {
-            unset($_GET['activate']);
-        }
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
 
-        $message = sprintf(
-        /* 1: Plugin name 2: Elementor 3: Required Elementor version */
-            esc_html__('"%1$s" requires "%2$s" version %3$s or greater.', 'lkc-elementor-widgets'),
-            '<strong>' . esc_html__('Lkc Elementor Widgets', 'lkc-elementor-widgets') . '</strong>',
-            '<strong>' . esc_html__('Elementor', 'lkc-elementor-widgets') . '</strong>',
-            self::MINIMUM_ELEMENTOR_VERSION
-        );
-        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+		$message = sprintf(
+		/* 1: Plugin name 2: Elementor 3: Required Elementor version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'lkc-elementor-widgets' ),
+			'<strong>' . esc_html__( 'Lkc Elementor Widgets', 'lkc-elementor-widgets' ) . '</strong>',
+			'<strong>' . esc_html__( 'Elementor', 'lkc-elementor-widgets' ) . '</strong>',
+			self::MINIMUM_ELEMENTOR_VERSION
+		);
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 
-    }
+	}
 
-    /**
-     * Admin notice
-     *
-     * Warning when the site doesn't have a minimum required PHP version.
-     *
-     * @since 1.0.0
-     *
-     * @access public
-     */
-    public function admin_notice_minimum_php_version()
-    {
+	/**
+	 * Admin notice
+	 *
+	 * Warning when the site doesn't have a minimum required PHP version.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function admin_notice_minimum_php_version() {
 
-        if (isset($_GET['activate'])) {
-            unset($_GET['activate']);
-        }
+		if ( isset( $_GET['activate'] ) ) {
+			unset( $_GET['activate'] );
+		}
 
-        $message = sprintf(
-        /* 1: Plugin name 2: PHP 3: Required PHP version */
-            esc_html__('"%1$s" requires "%2$s" version %3$s or greater.', 'lkc-elementor-widgets'),
-            '<strong>' . esc_html__('Lkc Elementor Widgets', 'lkc-elementor-widgets') . '</strong>',
-            '<strong>' . esc_html__('PHP', 'lkc-elementor-widgets') . '</strong>',
-            self::MINIMUM_PHP_VERSION
-        );
+		$message = sprintf(
+		/* 1: Plugin name 2: PHP 3: Required PHP version */
+			esc_html__( '"%1$s" requires "%2$s" version %3$s or greater.', 'lkc-elementor-widgets' ),
+			'<strong>' . esc_html__( 'Lkc Elementor Widgets', 'lkc-elementor-widgets' ) . '</strong>',
+			'<strong>' . esc_html__( 'PHP', 'lkc-elementor-widgets' ) . '</strong>',
+			self::MINIMUM_PHP_VERSION
+		);
 
-        printf('<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message);
+		printf( '<div class="notice notice-warning is-dismissible"><p>%1$s</p></div>', $message );
 
-    }
+	}
 
-    /**
-     * Enqueues basic widgets for registration
-     *
-     * @return void
-     */
-    public function enqueue_widgets()
-    {
-        //Require widget files
-        $this->require_widget_files();
-		
-        // Register Basic Widgets
-        add_action('elementor/widgets/widgets_registered', [$this, 'register_widgets']);
+	/**
+	 * Enqueues basic widgets for registration
+	 *
+	 * @return void
+	 */
+	public function enqueue_widgets() {
+		//Require widget files
+		$this->require_widget_files();
 
-        // Register Basic Widget Styles
-        add_action('elementor/frontend/after_enqueue_styles', [$this, 'enqueue_widget_styles']);
-    }
+		// Register Basic Widgets
+		add_action( 'elementor/widgets/widgets_registered', [ $this, 'register_widgets' ] );
 
-    /**
-     * Require basic widget files
-     *
-     * @return void
-     */
-    public function require_widget_files()
-    {
+		// Register Basic Widget Styles
+		add_action( 'elementor/frontend/after_enqueue_styles', [ $this, 'enqueue_widget_styles' ] );
+	}
 
-    }
+	/**
+	 * Require basic widget files
+	 *
+	 * @return void
+	 */
+	public function require_widget_files() {
+		require_once( 'widgets/cinema-movies-showcase-widget.php' );
+	}
 
-    /**
-     * Registers basic widgets
-     *
-     * @param Widgets_Manager $widget_manager
-     *
-     * @return void
-     */
-    public function register_widgets(Widgets_Manager $widget_manager)
-    {
+	/**
+	 * Registers basic widgets
+	 *
+	 * @param Widgets_Manager $widget_manager
+	 *
+	 * @return void
+	 */
+	public function register_widgets( Widgets_Manager $widget_manager ) {
+		$widget_manager->register( new Cinema_Movies_Showcase_Widget() );
+	}
 
-    }
+	/**
+	 * Enqueues basic widget styles
+	 *
+	 * @return void
+	 */
+	public function enqueue_widget_styles() {
+		wp_enqueue_style(
+			'lkc-elementor-widgets-stylesheet',
+			plugins_url( '/assets/css/lkc-elementor-widgets.css', LKC_PLUGIN_FILE )
+		);
+	}
 
-    /**
-     * Enqueues basic widget styles
-     *
-     * @return void
-     */
-    public function enqueue_widget_styles()
-    {
-        wp_enqueue_style(
-            'lkc-elementor-widgets-stylesheet',
-            plugins_url('/assets/css/lkc-elementor-widgets.css', LKC_PLUGIN_FILE)
-        );
-    }
-
-    /**
-     * Register custom categories
-     *
-     * @void
-     */
-    public function register_widget_categories($elements_manager)
-    {
-        $elements_manager->add_category(
-            'lkc',
-            array(
-                'title' => esc_html__('LKC', 'lkc'),
-                'icon' => 'fa fa-plug',
-            )
-        );
-    }
+	/**
+	 * Register custom categories
+	 *
+	 * @void
+	 */
+	public function register_widget_categories( $elements_manager ) {
+		$elements_manager->add_category(
+			'lkc',
+			array(
+				'title' => esc_html__( 'LKC', 'lkc' ),
+				'icon'  => 'fa fa-plug',
+			)
+		);
+	}
 
 }
 
 
-add_action('init', 'lkc_elementor_init');
-function lkc_elementor_init()
-{
-    Lkc_Elementor_Widgets::get_instance();
+add_action( 'init', 'lkc_elementor_init' );
+function lkc_elementor_init() {
+	Lkc_Elementor_Widgets::get_instance();
 }
 
---
+++ widgets/cinema-movies-showcase-widget.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Elementor;
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+/**
+ * Elementor Cinema Movies Showcase Widget
+ *
+ * Elementor widget that displays movie cards. Movies are fetched from https://bioskoplkc.rs API and
+ * three latest movies are display using this widget.
+ *
+ */
+class Cinema_Movies_Showcase_Widget extends Widget_Base {
+
+	/**
+	 * Get widget name.
+	 *
+	 * Retrieve Cinema Movies Showcase widget name.
+	 *
+	 * @return string Widget name.
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function get_name(): string {
+		return 'cinema-movies-showcase';
+	}
+
+	/**
+	 * Get widget title.
+	 *
+	 * Retrieve Cinema Movies Showcase widget title.
+	 *
+	 * @return string Widget title.
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function get_title(): string {
+		return esc_html__( 'Cinema Movies Showcase', 'lkc-elementor-widgets' );
+	}
+
+	/**
+	 * Get widget icon.
+	 *
+	 * Retrieve Cinema Movies Showcase widget icon.
+	 *
+	 * @return string Widget icon.
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function get_icon(): string {
+		return 'eicon-testimonial-carousel';
+	}
+
+	/**
+	 * Get widget categories.
+	 *
+	 * Retrieve the list of categories the Cinema Movies Showcase widget belongs to.
+	 *
+	 * @return array Widget categories.
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function get_categories(): array {
+		return array( 'lkc' );
+	}
+
+	/**
+	 * Get widget keywords.
+	 *
+	 * Retrieve the list of keywords the Cinema Movies Showcase widget belongs to.
+	 *
+	 * @return array Widget keywords.
+	 * @since 1.0.0
+	 * @access public
+	 */
+	public function get_keywords(): array {
+		return array( 'movies', 'movie', 'cinema' );
+	}
+
+	/**
+	 * Register Cinema Movies Showcase widget controls.
+	 *
+	 * Add input fields to allow the user to customize the widget settings.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function register_controls() {}
+
+	/**
+	 * Render Cinema Movies Showcase widget output on the frontend.
+	 *
+	 * Written in PHP and used to generate the final HTML.
+	 *
+	 * @since 1.0.0
+	 * @access protected
+	 */
+	protected function render() {
+		$movies = $this->get_three_latest_movies();
+
+		echo '<div class="cinema-movies-showcase-container">';
+		echo ' <div class="cinema-movies-showcase-wrapper">';
+		foreach ( $movies as $movie ):
+			echo "<a href=\"$movie->permalink\" style=\"background: url('$movie->img_url')\" class=\"cinema-movies-showcase-card\">";
+			echo '<div class="cinema-movies-showcase-card__overlay"></div>';
+			echo "<h3 class=\"cinema-movies-showcase-card__title\">$movie->post_title</h3>";
+			echo '</a>';
+		endforeach;
+		echo '</div>';
+		echo '</div>';
+	}
+
+
+	/**
+	 * Returns array of three latest movies fetched from 'https://bioskoplkc.rs' API.
+	 *
+	 * @return array
+	 */
+	private function get_three_latest_movies(): array {
+		$response = wp_remote_get( 'https://bioskoplkc.rs/wp-json/cinema/v1/latest-movies' );
+		$data     = wp_remote_retrieve_body( $response );
+
+		return json_decode( $data );
+	}
+}
