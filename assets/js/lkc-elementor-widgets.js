338c41176a98a93016215bfdf1e4e7b8bd43a86a
Miljan Jovic <miljankinez@gmail.com>
2022-04-09 03:36:10 +0200

    News Widget Responsive - Fixed

--- assets/js/lkc-elementor-widgets.js
+++ assets/js/lkc-elementor-widgets.js
@@ -14,6 +14,23 @@ $jq(document).ready(function () {
         slidesToScroll: 3,
         appendArrows: $jq('.news-widget__arrows'),
         prevArrow: '<button type="button" class="slick-prev"><i class="fa-solid fa-chevron-left"></i></button>',
-        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>'
+        nextArrow: '<button type="button" class="slick-next"><i class="fa-solid fa-chevron-right"></i></button>',
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                    infinite: true,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
+            }
+        ]
     });
 });
